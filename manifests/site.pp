f67c8f62564f9c4aff5cf43a804d216114a8219a
Franklin88 <morpheus_cisco@hotmail.com>
2019-02-27 18:00:54 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,4 +5,10 @@ node default {
     owner   => 'root',
     mode    => '0600',
   }
+  file {'/root/README2':
+    ensure  => file,
+    content => "This is a README file\n",
+    owner   => 'root',
+    mode    => '0600',
+  }
 }
