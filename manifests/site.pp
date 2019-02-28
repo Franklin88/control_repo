c32813141f0b6826faf187bd9398e5a18adab86f
Franklin88 <morpheus_cisco@hotmail.com>
2019-02-28 12:40:21 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node master.puppet.vm {
   include role::master_server
   file {'/root/README':
     ensure  => file,
-    content => "${fqdn}\n",
+    content => "${fqdn}\n${os}\n",
     owner   => 'root',
     mode    => '0600',
   }
